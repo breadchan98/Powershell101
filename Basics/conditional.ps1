f30737d10a4a0c7c8205a01a14a2846b91d68b2a
Brent Buenarte <buenartebrent98@gmail.com>
2025-07-26 15:59:58 -0700

    nested statements

--- Basics/conditional.ps1
+++ Basics/conditional.ps1
@@ -18,3 +18,42 @@
 
 # -ccontains makes it case-sensitive
 @('Test','test') -ccontains 'TEST'
+
+#This is for extra example use of conditional (file path is under linux dir)
+$pathFile = "/home/bred/Desktop/PowerShell101/Powershell101/Basics/Data Reading/test.txt"
+Test-Path -Path $pathFile
+
+# Conditional Statement Script Example
+
+#if the following statement is true, the script executes
+if(Test-Path -Path $pathFile) {
+    #output the string
+    Write-Output "The File Exist Here"
+
+    #output the contents of file
+    Get-Content -Path $pathFile
+}
+else {
+    #For referencing variable paths, use double quotes on strings
+    Write-Output "File '$pathFile' does not exist!"
+}
+
+### Nested If-Else Statements
+$pathFile1 = "/home/bred/Desktop/PowerShell101/Powershell101/Basics/Data Reading/test1.txt"
+
+if(Test-Path -Path $pathFile1) {
+    $Data = Get-Content -Path $pathFile1
+    if($Data.count -lt 5) {
+        Write-Output "This file has less than five lines"
+    }
+    elseif ($Data.Count -lt 2) {
+        Write-Output "This file has less two lines"
+    }
+    else {
+        Write-Output "There's a lot of lines here lol"
+    }
+    Write-Output "Statement completed!"
+}
+else {
+    Write-Output = "File '$pathFile1' does not exist!"
+}
