d7e2a7381785f1b9f1d1ef60b9df840c392ba44a
breadchan98 <buenartebrent98@gmail.com>
2025-07-25 23:58:13 -0700

    full array basics

--- Basics/arrays.ps1
+++ Basics/arrays.ps1
@@ -58,7 +58,40 @@ $myArray
     ArrayList > Array, especially if you dont know how big is your array
     and to prevent any performance hiccups
 
-    To declare an ArrayList:
-    $listVar=[System.Collections.ArrayList]@()
-
+    To declare an ArrayList, there are 2 methods:
+    1. $listVar=[System.Collections.ArrayList]@()
+    2. #listVar=New-Object -TypeName Systems.Collections.ArrayList
+        --The second one is recommended, especially if having huge data
 #>
+
+#Declaring Array List
+$myList1 = New-Object -TypeName System.Collections.ArrayList #just creating the list
+
+#Adding an item in arraylist using .Add() since it is not a fixed size array
+$myList1.Add("CPU")
+
+#adding [void] will null the item and make it not output the index as it usually does in ps
+[void]$myList1.Add("RAM")
+
+#.AddRange(@()) lets you add more items in the array.. very neat if you dont wanna keep copy and paste .Add() per line
+
+$myList1.AddRange(@("SDD", "PSU", "GPU"))
+
+<#
+    Removal Methods:
+    1. .Remove(item) - remove a specific item
+    2. .RemoveAt(index) - remove at a specific index
+    3. .RemoveRange(startIndex, endIndex) - remove at a starting and ending index
+#>
+
+#Creating ArrayList
+$gunList = New-Object -TypeName System.Collections.ArrayList
+$gunList.AddRange(@("Ray Gun", "Winter's Howl", "Scavenger", "VR11", "Wunderwaffe DG-2", "Wave Gun", "Thundergun"))
+
+#For removing items
+$gunList.Remove("Thundergun")
+$gunList.RemoveAt(2)
+$gunList.RemoveRange(0,3)
+
+#Final Result
+$gunList
