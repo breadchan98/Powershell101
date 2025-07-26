5b69bf1b55a96a92538266d9dfa80b2c03a0048b
Brent Buenarte <buenartebrent98@gmail.com>
2025-07-26 16:26:25 -0700

    for each practice

--- Basics/Loops Practice/foreach.ps1
+++ Basics/Loops Practice/foreach.ps1
@@ -3,3 +3,17 @@
     - Used for repeating a process, "iterating"
     - There are three methods to iterate using loops, per performance wise
 #>
+
+#Goal: Create folders and save via Shared Folders WITHOUT doing it manually
+
+$pathName = "Basics/Loops Practice/FolderNameRequests.txt"
+$targetPath = "Basics/Loops Practice/Shared Folders"
+
+#This will treat the var as an array, can be used now for indexing
+$folderNames = Get-Content -Path $pathName
+
+<#
+- New-Item -Path "target path" -Name arrayVar(ex: folderNames)[index] -ItemType Directory
+#>
+
+New-Item -Path "$targetPath" -Name $folderNames[0] -ItemType Directory
