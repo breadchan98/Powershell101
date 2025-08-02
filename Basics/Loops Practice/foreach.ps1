b6d3a176e8a075484a34b195b0e5b2eddc4be75b
breadchan98 <buenartebrent98@gmail.com>
2025-08-02 01:04:07 -0700

    for each prac cont

--- Basics/Loops Practice/foreach.ps1
+++ Basics/Loops Practice/foreach.ps1
@@ -16,4 +16,43 @@ $folderNames = Get-Content -Path $pathName
 - New-Item -Path "target path" -Name arrayVar(ex: folderNames)[index] -ItemType Directory
 #>
 
+<#
+Reference, the old way!
+
+#Manual Way
 New-Item -Path "$targetPath" -Name $folderNames[0] -ItemType Directory
+New-Item -Path "$targetPath" -Name $folderNames[1] -ItemType Directory
+New-Item -Path "$targetPath" -Name $folderNames[2] -ItemType Directory
+
+#>
+
+#Foreach Test Practice
+
+############## THE FIRST METHOD #######################
+
+#for each var (lines) in the folderNames (the content in pathName), it will print out each
+foreach($line in $folderNames) {
+    #if the path SharedFolders\name of user does not exist
+    if((Test-Path -Path "$targetPath\$line") -eq $false) {
+        #create new item on targetPath named $line
+        New-Item -Path "$targetPath" -Name $line -ItemType Directory
+    }
+    else {
+        Write-Output "This Folder Exist"
+    }
+}
+
+############## THE SECOND METHOD #######################
+<#
+    For-Each Object Cmdlet and Pipeline
+#>
+$folderNames | ForEach-Object -Process {
+    # $line to $_
+    if((Test-Path -Path "$targetPath\$_") -eq $false) {
+        #create new item on targetPath named $line
+        New-Item -Path "$targetPath" -Name $_ -ItemType Directory
+    }
+    else {
+        Write-Output "This Folder Exist"
+    }
+}
