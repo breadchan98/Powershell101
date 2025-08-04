92cbc2c5973a46f30f1a36587b12360a6eb6f2b7
Brent Buenarte <69672235+breadchan98@users.noreply.github.com>
2025-08-04 14:23:55 -0700

    Update UUID-MAC.ps1

--- Projects/UUID-MAC.ps1
+++ Projects/UUID-MAC.ps1
@@ -8,10 +8,20 @@ New-PSDrive -Name "Z" -PSProvider FileSystem -Root "\\mlsredfs\Teams\v-bbuenarte
 $machineName = Read-Host "Enter name of machine"
 New-Item -Path "Z:\RITM2438749\$machineName.txt" -ItemType File
 
+#Creating separate mac log for extracting
+$ipAddress = cmd.exe /c "ipconfig /all"
+New-Item -Path "Z:\RITM2438749\MAC logs\$machineName.MAC.txt" -ItemType File
+Add-Content -Path "Z:\RITM2438749\MAC logs\$machineName.MAC.txt" -Value $ipAddress
+
+#Finds pattern string
+$macAddress = Get-Content "Z:RITM2438749\MAC logs\$machineName.MAC.txt" | Select-String -Pattern "Physical","Description"
+
 #Show UUID and MAC
 $uuidMacLines = @(
     (Get-CimInstance Win32_ComputerSystemProduct).UUID
     " "
-    cmd.exe /c "wmic nic get Caption,MACAddress"
+    $macAddress
     )
 Add-Content -Path "Z:\RITM2438749\$machineName.txt" -Value $uuidMacLines
+
+cmd.exe /c "net use Z: /delete"
