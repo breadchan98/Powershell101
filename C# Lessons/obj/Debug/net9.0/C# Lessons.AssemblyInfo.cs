bdfb0bc92146b2535d7cff8927c229f4e6ba4828
breadchan98 <buenartebrent98@gmail.com>
2025-11-10 23:50:05 -0800

    assembly info change

--- C# Lessons/obj/Debug/net9.0/C# Lessons.AssemblyInfo.cs
+++ C# Lessons/obj/Debug/net9.0/C# Lessons.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C# Lessons")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deddcf4c0b4e2371c471677ea36e3a50968a9182")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+873c0c31d8ccb441f286c2639593c0ae86b67223")]
 [assembly: System.Reflection.AssemblyProductAttribute("C# Lessons")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C# Lessons")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
