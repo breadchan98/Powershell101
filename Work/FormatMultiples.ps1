446b72ab12a04f4f80b2142f2ae15d923c51d024
Brent Buenarte <buenartebrent98@gmail.com>
2026-01-12 09:13:53 -0800

    format only

--- Work/FormatMultiples.ps1
+++ Work/FormatMultiples.ps1
@@ -1,10 +1,6 @@
 Set-StrictMode -Version Latest
 
-$cred = Get-Credential -Message "Enter Credentials"
-$driveLetter = Read-Host "Enter Drive Letter for Network Path"
-New-PSDrive -Name "$driveLetter" -PSProvider FileSystem -Root "\\sesdfs\1windows\TestContent\CORE\Base\ES\Ops\ScoreEdge\Devices\Surface\SP11\Configure\DevOS\Staging\Cadmus_USB_Network_Recovery_V4.2_NoWTT_SP11XGA_01052026" -Credential $cred -Persist
-
-$connectedDrives = (Read-Host "Enter drive letters (comma-separated") -split '\s+'
+$connectedDrives = (Read-Host "Enter drive letters (space-separated)") -split '\s+'
 
 $confirm = Read-Host "Format all drives? Type Y to Continue or press any key to skip"
 
@@ -15,20 +11,5 @@ if ($confirm -eq "N") {
 if ($confirm -eq "Y") {
     foreach ($drive in $connectedDrives) {
         Format-Volume -DriveLetter $drive -FileSystem FAT32 -NewFileSystemLabel BOOTME -Confirm:$true
-    }
-}
-
-
-while($true) {
-    $formatDrive = Read-Host "Ready to add files to drives? Type Y to continue or N to not"
-    if ($formatDrive -eq 'N') {
-        break
-    }
-    if ($formatDrive -eq 'Y') {
-        foreach ($driveL in $connectedDrives) {
-            $source = "${driveLetter}:\"
-            $target = "${driveL}:\"
-            robocopy $source $target /E
-        }
     }
 }
