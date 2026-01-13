2f23ebf17d2a4b3f7d6bc8aa531c865e1c24cb69
Brent Buenarte <buenartebrent98@gmail.com>
2026-01-12 16:17:26 -0800

    idk

--- Work/Untitled.ps1
+++ Work/Untitled.ps1
@@ -1,1 +1,13 @@
 Set-StrictMode -Version Latest
+
+$input = Read-Host "1 2 or 3"
+
+if ($input -eq "1") {
+    Write-Host "Stopping. Double check first."
+    break
+}
+if ($input -eq "3") {
+    Write-Host "dude"
+}
+
+Write-Host "Hello World"
