4299b80e5162d8e6792058da3e36a9b0f104eced
Brent Buenarte <buenartebrent98@gmail.com>
2026-01-16 16:11:32 -0800

    parallel test

--- Work/ParallelCPU.ps1
+++ Work/ParallelCPU.ps1
@@ -2,6 +2,7 @@ Set-StrictMode -Version Latest
 
 <#
 Practice Parallel CPU usages
+$_ = current pipeline
 #>
 
 #Microsoft Learn
@@ -14,6 +15,26 @@ $folderNames = @(
     'Fortnite'
 )
 $folderNames | ForEach-Object -Parallel {
-    New-Item -Path .\Work -Name $folderNames -ItemType File
+    New-Item -Path '.\Work\Test' -Name $_ -ItemType File
     sleep 1;
+} -ThrottleLimit 3
+
+#Test with UserInput
+$connectedDrives = (Read-Host "Enter drive letters (space separated)") -split '\s+'
+
+$connectedDrives | ForEach-Object -Parallel {
+    New-Item -Path '.\Work\Test' -Name "$_.txt" -ItemType File
+    Add-Content -Path ".\Work\Test\$_.txt" -Value "hello"
+    Start-Sleep 1;
+} -ThrottleLimit 3
+
+#Format multiple drives at once
+$driveNames = @(
+    'E',
+    'F',
+    'G'
+)
+$driveNames | ForEach-Object -Parallel {
+    Format-Volume -DriveLetter $_ -FileSystem FAT32 -NewFileSystemLabel BOOTME -Confirm:$true
+    Start-Sleep 1;
 } -ThrottleLimit 3
